d4b07239e773277109073dbca62c96a56efa2d09
Виталий Грахов <v.a.grahov@yandex.ru>
2024-03-01 11:55:41 +0500

    task4 Shop

---
+++ src/main/java/ru/gravitana/core/task4/Item.java
@@ -0,0 +1,24 @@
+package ru.gravitana.core.task4;
+
+/**
+ * "Товар"
+ */
+public class Item {
+
+    String name;
+    double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                "Наименование: '" + name + '\'' +
+                ", Цена: " + price +
+                ']';
+    }
+
+}
---
+++ src/main/java/ru/gravitana/core/task4/exceptions/AmountException.java
@@ -0,0 +1,7 @@
+package ru.gravitana.core.task4.exceptions;
+
+public class AmountException extends Exception {
+    public AmountException(String message) {
+        super(message);
+    }
+}
